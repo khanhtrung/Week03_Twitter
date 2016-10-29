8ff00fd74783e7aa9bc477f284e4d977822194d2
Tran Khanh Trung <khanhtrung90@yahoo.com>
2016-10-29 23:42:16 +0700

    Update code

---
+++ Twitter/TwitterDefaults.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterDefaults.swift
+//  Twitter
+//
+//  Created by Tran Khanh Trung on 10/29/16.
+//  Copyright © 2016 TRUNG. All rights reserved.
+//
+
+import UIKit
+
+class TwitterDefaults: NSObject {
+
+}
